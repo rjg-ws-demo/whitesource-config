9906b5f174abe7152f2658345320f47d7404de93
Randy Geyer <randy.geyer@whitesourcesoftware.com>
2021-05-27 22:54:15 +0000

    chore: add deploy.sh

---
+++ docker/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+WSS=/home/ubuntu/ghe/v21.5.1
+if (( $# == 1 ))
+then
+    WSS=$1
+fi
+echo $WSS
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $DIR
+cp $DIR/docker-compose.yaml $WSS/../docker-compose.yaml
+cp $DIR/conf/prop.json $WSS/../conf/prop.json
+cp $DIR/scanner/Dockerfile $WSS/wss-scanner/docker/Dockerfile
